d3522e6f7f0c9c7f8a4c92225213ab75ce6f1a2c
Julen Piris <jpiris@gureak.com>
2021-08-30 23:33:31 +0200

    Move the GildedRose class content to UpdatableItem class

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -4,15 +4,6 @@ namespace GildedRoseKata
 {
     public class GildedRose
     {
-        private const string AGED_BRIE = "Aged Brie";
-        private const string BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
-        private const string SULFURAS = "Sulfuras, Hand of Ragnaros";
-        private const int MAXIMUM_QUALITY = 50;
-        private const int MINIMUM_QUALITY = 0;
-        private const int DOUBLE_BACKSTAGE_PASSES_QUALITY_WHEN_SELL_IN_LOWER_THAN = 11;
-        private const int TRIPLE_BACKSTAGE_PASSES_QUALITY_WHEN_SELL_IN_LOWER_THAN = 6;
-        private const int DECREASE_DEFAULT_ITEM_QUALITY_BY_TWO_WHEN_SELL_IN_LOWER_THAN = 0;
-
         private readonly IList<Item> _items;
 
         public GildedRose(IList<Item> Items)
@@ -24,93 +15,9 @@ namespace GildedRoseKata
         {
             foreach (var item in _items)
             {
-                switch (item.Name)
-                {
-                    case AGED_BRIE:
-                        DecreaseSellIn(item);
-                        IncreaseAgedBrieQuality(item);
-                        break;
-                    case BACKSTAGE_PASSES:
-                        IncreaseBackstagePassesQuality(item);
-                        DecreaseSellIn(item);
-                        DropBackstagePassesQualityToZero(item);
-                        break;
-                    case SULFURAS:
-                        break;
-                    default:
-                        DecreaseSellIn(item);
-                        DecreaseDefaultItemQuality(item);
-                        break;
-                }
-            }
-        }
+                var updatableItem = new UpdatableItem(item);
 
-        private void DropBackstagePassesQualityToZero(Item item)
-        {
-            if (item.SellIn < DECREASE_DEFAULT_ITEM_QUALITY_BY_TWO_WHEN_SELL_IN_LOWER_THAN)
-            {
-                DropQualityToZero(item);
-            }
-        }
-
-        private void DecreaseDefaultItemQuality(Item item)
-        {
-            DecreaseQuality(item);
-
-            if (item.SellIn < DECREASE_DEFAULT_ITEM_QUALITY_BY_TWO_WHEN_SELL_IN_LOWER_THAN)
-            {
-                DecreaseQuality(item);
-            }
-        }
-
-        private void IncreaseBackstagePassesQuality(Item item)
-        {
-            IncreaseQuality(item);
-
-            if (item.SellIn < DOUBLE_BACKSTAGE_PASSES_QUALITY_WHEN_SELL_IN_LOWER_THAN)
-            {
-                IncreaseQuality(item);
-            }
-
-            if (item.SellIn < TRIPLE_BACKSTAGE_PASSES_QUALITY_WHEN_SELL_IN_LOWER_THAN)
-            {
-                IncreaseQuality(item);
-            }
-        }
-
-        private void IncreaseAgedBrieQuality(Item item)
-        {
-            IncreaseQuality(item);
-
-            if (item.SellIn < DECREASE_DEFAULT_ITEM_QUALITY_BY_TWO_WHEN_SELL_IN_LOWER_THAN)
-            {
-                IncreaseQuality(item);
-            }
-        }
-
-        private void DropQualityToZero(Item item)
-        {
-            item.Quality = 0;
-        }
-
-        private void DecreaseSellIn(Item item)
-        {
-            item.SellIn--;
-        }
-
-        private void DecreaseQuality(Item item)
-        {
-            if (item.Quality > MINIMUM_QUALITY)
-            {
-                item.Quality--;
-            }
-        }
-
-        private void IncreaseQuality(Item item)
-        {
-            if (item.Quality < MAXIMUM_QUALITY)
-            {
-                item.Quality++;
+                updatableItem.Update();
             }
         }
     }
---
+++ csharpcore/GildedRose/UpdatableItem.cs
@@ -0,0 +1,112 @@
+﻿namespace GildedRoseKata
+{
+    public class UpdatableItem
+    {
+        private const string AGED_BRIE = "Aged Brie";
+        private const string BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+        private const string SULFURAS = "Sulfuras, Hand of Ragnaros";
+        private const int MAXIMUM_QUALITY = 50;
+        private const int MINIMUM_QUALITY = 0;
+        private const int DOUBLE_BACKSTAGE_PASSES_QUALITY_WHEN_SELL_IN_LOWER_THAN = 11;
+        private const int TRIPLE_BACKSTAGE_PASSES_QUALITY_WHEN_SELL_IN_LOWER_THAN = 6;
+        private const int DECREASE_DEFAULT_ITEM_QUALITY_BY_TWO_WHEN_SELL_IN_LOWER_THAN = 0;
+
+        private readonly Item _item;
+
+        public UpdatableItem(Item item)
+        {
+            _item = item;
+        }
+
+        public void Update()
+        {
+            switch (_item.Name)
+            {
+                case AGED_BRIE:
+                    DecreaseSellIn();
+                    IncreaseAgedBrieQuality();
+                    break;
+                case BACKSTAGE_PASSES:
+                    IncreaseBackstagePassesQuality();
+                    DecreaseSellIn();
+                    DropBackstagePassesQualityToZero();
+                    break;
+                case SULFURAS:
+                    break;
+                default:
+                    DecreaseSellIn();
+                    DecreaseDefaultItemQuality();
+                    break;
+            }
+        }
+
+        private void DropBackstagePassesQualityToZero()
+        {
+            if (_item.SellIn < DECREASE_DEFAULT_ITEM_QUALITY_BY_TWO_WHEN_SELL_IN_LOWER_THAN)
+            {
+                DropQualityToZero();
+            }
+        }
+
+        private void DecreaseDefaultItemQuality()
+        {
+            DecreaseQuality();
+
+            if (_item.SellIn < DECREASE_DEFAULT_ITEM_QUALITY_BY_TWO_WHEN_SELL_IN_LOWER_THAN)
+            {
+                DecreaseQuality();
+            }
+        }
+
+        private void IncreaseBackstagePassesQuality()
+        {
+            IncreaseQuality();
+
+            if (_item.SellIn < DOUBLE_BACKSTAGE_PASSES_QUALITY_WHEN_SELL_IN_LOWER_THAN)
+            {
+                IncreaseQuality();
+            }
+
+            if (_item.SellIn < TRIPLE_BACKSTAGE_PASSES_QUALITY_WHEN_SELL_IN_LOWER_THAN)
+            {
+                IncreaseQuality();
+            }
+        }
+
+        private void IncreaseAgedBrieQuality()
+        {
+            IncreaseQuality();
+
+            if (_item.SellIn < DECREASE_DEFAULT_ITEM_QUALITY_BY_TWO_WHEN_SELL_IN_LOWER_THAN)
+            {
+                IncreaseQuality();
+            }
+        }
+
+        private void DropQualityToZero()
+        {
+            _item.Quality = 0;
+        }
+
+        private void DecreaseSellIn()
+        {
+            _item.SellIn--;
+        }
+
+        private void DecreaseQuality()
+        {
+            if (_item.Quality > MINIMUM_QUALITY)
+            {
+                _item.Quality--;
+            }
+        }
+
+        private void IncreaseQuality()
+        {
+            if (_item.Quality < MAXIMUM_QUALITY)
+            {
+                _item.Quality++;
+            }
+        }
+    }
+}
